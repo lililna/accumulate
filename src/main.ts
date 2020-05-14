05247ad43c08a4bd3c100c412ccd956886e0c08b
liqiaona <lqnstar@163.com>
2020-05-14 09:58:18 +0800

    注释报错部分

--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,12 @@ const bootstrap = () => {
 };
 
 if (environment.hmr) {
-  if (module['hot']) {
+  /* if (module['hot']) {
     hmrBootstrap(module, bootstrap);
   } else {
     console.error('HMR is not enabled for webpack-dev-server!');
     console.log('Are you using the --hmr flag for ng serve?');
-  }
+  } */
 } else {
   bootstrap().then(() => {
     if ((<any>window).appBootstrap) {
